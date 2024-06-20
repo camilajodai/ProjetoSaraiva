27339ddbcab9a042ef4da7dd4d0d83233b83974c
Camila Jodai <camijodai@gmail.com>
2024-06-20 15:01:24 -0300

    Feature: Finalização do conteúdo da rota personaldata

--- usuarios/src/routes/personaldata/personaldata.js
+++ usuarios/src/routes/personaldata/personaldata.js
@@ -2,8 +2,9 @@ require("dotenv").config()
 const express = require("express")
 const routerpersonal = express.Router()
 const data = require("../../database/config.js")
+const verificar = require("../../middleware/verify_token.js")
 
-routerpersonal.get("/listar", (req, res) => {
+routerpersonal.get("/listar", verificar, (req, res) => {
     data.query("select * from dadospessoais", (error, result) => {
         if (error) {
             return res.status(500).send({ msg: "Erro ao ler os dados" })
@@ -12,7 +13,7 @@ routerpersonal.get("/listar", (req, res) => {
     })
 })
 
-routerpersonal.get("/listar:cpf", (req, res) => {
+routerpersonal.get("/listar:cpf", verificar, (req, res) => {
     data.query("select * from dadospessoais where iddadospessoais=?", req.params.cpf, (error, result) => {
         if (error) {
             return res.status(500).send({ msg: "Erro ao tentar carregar os dados" })
@@ -21,7 +22,7 @@ routerpersonal.get("/listar:cpf", (req, res) => {
     })
 })
 
-routerpersonal.post("/cadastrar", (req, res) => {
+routerpersonal.post("/cadastrar", verificar, (req, res) => {
     data.query("insert into dadospessoais set ?", req.body, (error, result) => {
         if (error) {
             return res.status(500).send({ msg: "Erro ao tentar cadastrar" })
@@ -29,3 +30,15 @@ routerpersonal.post("/cadastrar", (req, res) => {
         res.status(201).send({ msg: "Ok", payload: result })
     })
 })
+
+
+routerpersonal.put("/atualizar/:id", verificar, (req, res) => {
+    data.query("update dadospessoais set ? where iddadospessoais=?", [req.body, req.params.id], (error, result) => {
+        if (error) {
+            return res.status(500).send({ msg: "Erro ao tentar atualizar" })
+        }
+        res.status(200).send({ msg: "Ok", payload: result })
+    })
+})
+
+module.exports = routerpersonal
