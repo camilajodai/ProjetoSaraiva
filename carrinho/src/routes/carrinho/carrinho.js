21ed9b283363b3cf0cebb357c46770e72613a720
Camila Jodai <camijodai@gmail.com>
2024-07-24 17:06:32 -0300

    mudanças

--- carrinho/src/routes/carrinho/carrinho.js
+++ carrinho/src/routes/carrinho/carrinho.js
@@ -11,4 +11,27 @@ route_carrinho.get("/listar", (req, res) => {
     })
 })
 
+route_carrinho.post("/adicionar", (req,res) => {
+    data.query("insert into carrinho set ?", req.body, (error, result) => {
+        if (error) {
+            return res.status(500).send({ msg: "Erro ao tentar adicionar no carrinho." })
+        }
+        res.status(201).send({ msg: "Ok", payload: result })
+    })
+})
+
+route_carrinho.get("/listar/:id", (req, res) => {
+    data.query(`SELECT foto.foto1,titulo.nometitulo,titulo.autor,carrinho.quantidade,
+    preco.precodesconto,titulo.idtitulo,carrinho.total
+    FROM saraivalivrodb.fotos foto INNER JOIN saraivalivrodb.titulos titulo
+    ON foto.idfoto=titulo.idfoto INNER JOIN saraivacarrinhodb.carrinho carrinho
+    ON titulo.idtitulo=carrinho.idproduto INNER JOIN saraivalivrodb.precos preco 
+    ON preco.idpreco = titulo.idpreco WHERE carrinho.idusuario=?`, req.params.id, (error, dados) => {
+        if (error) {
+            return res.status(500).send({ msg: "Erro ao carregar os dados" })
+        }
+        res.status(200).send({ msg: "Ok", payload: dados })
+    })
+})
+
 module.exports = route_carrinho
